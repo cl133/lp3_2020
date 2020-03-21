7aad345ac0d9c368069ac07dd6b2ffcfff3dfef0
cl133 <48534157+cl133@users.noreply.github.com>
2020-03-20 22:25:46 -0300

    Add files via upload

---
+++ ejercicio1/Autor.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package herencia;
+
+/**
+ *
+ * @author laurachavez
+ */
+public class Autor extends Persona {
+    
+    public Autor(String nombre, String apellido, int edad){
+        super(nombre,apellido,edad);
+    }
+            
+            
+            
+}

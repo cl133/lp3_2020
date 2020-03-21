86645b2294e78271ea09aaf63dce56d33c505324
cl133 <48534157+cl133@users.noreply.github.com>
2020-03-20 22:22:38 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package herencia;
+
+/**
+ *
+ * @author laurachavez
+ */
+    public class Main {
+        
+        public static void main(String[] args) {
+             Editor editor1= new Editor("Juan","Perez",32);
+             editor1.Imprimir();
+        }
+    }
+   
+
+    
